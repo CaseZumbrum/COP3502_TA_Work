573dbe2d716771c741c11d9cc2127ee304f21007
casezumbrum <casezumbrum@ufl.edu>
2025-01-16 18:47:09 -0500

    add if demo

---
+++ Week 1/if.py
@@ -0,0 +1,12 @@
+t = True
+f = False
+x = 0
+y = 1
+z = 2
+
+if(True):
+    print("wow the first statement was true!!!")
+elif(False):
+    print("wow the second statement was true!!!")
+else:
+    print("neither statement was true :(")
