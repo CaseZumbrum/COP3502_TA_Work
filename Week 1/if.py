b59408bec1afdb32bcdd7de3ca131b29c4beb241
Case Zumbrum <casezumbrum@gmail.com>
2025-01-29 13:32:36 -0500

    setup week 2

--- Week 1/if.py
+++ Week 1/if.py
@@ -1,12 +1,19 @@
-t = True
-f = False
-x = 0
-y = 1
-z = 2
 
-if(True):
-    print("wow the first statement was true!!!")
-elif(False):
-    print("wow the second statement was true!!!")
+if(1 == 2):
+    print("this should not run")
+elif(2 == 2):
+    print("second level")
+elif(1 == 1):
+    print("the elif was true")
 else:
-    print("neither statement was true :(")
+    print("cool this ran")
+
+x = -1
+if  (x <= 0):
+    print('something')
+
+s1 = "hello"
+s2 = "hoifjaowie"
+
+if(s1 == "hello" or s2 == "hi"):
+    print("this worked")
---
+++ Week 2/turtle_demo.py
@@ -0,0 +1,17 @@
+import turtle
+
+l = 200
+
+turtle.penup()
+turtle.forward(l/2)
+turtle.pendown()
+turtle.speed(10)
+
+
+
+# starburst type shape
+while(True):
+    turtle.right(230)
+    turtle.forward(l)
+
+turtle.done()
