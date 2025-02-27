4335b5cd69aec34d200e2fbd452b74ca6a2bc7b4
Case Zumbrum <casezumbrum@gmail.com>
2025-02-26 21:48:33 -0500

    udpate week 5

--- Week 5/structures_demo.py
+++ Week 5/structures_demo.py
@@ -1,47 +1,43 @@
 # mypy: ignore-errors
 
 
-
 # iterate through a list
-l = [1,2,3,6,3423]
+l = [1, 2, 3, 6, 3423]
 print("------------------")
 print("LIST ITERATION")
 for val in l:
     print(val)
-    
+
 # list comprehension
 print("------------------")
 print("LIST COMPREHENSION")
-double = [2*val for val in l]
+double = [2 * val for val in l]
 for val in double:
     print(val)
 
 print("------------------")
 print("LIST ENUMERATION")
 
-for i, val in enumerate(l):
-    print(i, val)
-
 print("------------------")
 print("TUPLES VS LISTS")
 # tuples vs lists
-t = (1,3,4)
+t = (1, 3, 4)
 # t.append(5) # this will throw an error!!!!
+# t[1] = 0  # this will throw an error!!!
 print(t[2])
 
 print("------------------")
 print("SPREAD OPERATOR")
 
-t = (5,6,3,2,1,54)
+t = (5, 6, 3, 2, 1, 54)
 x1, x2, *x3, x4 = t
-print(x1,x2,x3,x4)
+print(x1, x2, x3, x4)
 
 print("------------------")
 print("ENUMERATION")
 
 for i, val in enumerate(t):
     print(i, val)
-
 
 
 print("------------------")
@@ -57,10 +53,10 @@ print(".get()", d.get("Mitsubishi", "default"))
 
 print("------------------")
 print("SETS")
-s = {4,65,2,1,1,1,1,1}
+s = {4, 65, 2, 1, 1, 1, 1, 1}
 print(s)
 s2 = {3}
 print(s.union(s2))
-#print(s[2]) # this will error!!!
+# print(s[2]) # this will error!!!
 print(3 in s)
 print(3 in s2)
